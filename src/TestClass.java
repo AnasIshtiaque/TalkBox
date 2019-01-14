bcefebbdc599797ad71f54c0b7c4713dcae6e41d
anas1325 <anas1325@ea08.eecs.yorku.ca>
2019-01-14 12:20:01 -0500

    commit my name line

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,5 +2,6 @@ public class TestClass {
 	public static void main(String args[]) {
 		System.out.println("Fun with Git"); //hi hello testing
 		System.out.println("Test Pushing."); //test pushing
+		System.out.println("Anas Ishtiaque"); //my name
 	}
 }
