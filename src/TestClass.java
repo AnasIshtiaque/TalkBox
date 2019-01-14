f9e9d119d28f3de3035828965e1a74f6bb712df1
Linwei-Wang <wanglw1994@gmail.com>
2019-01-14 12:16:37 -0500

    test push

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,6 @@
 public class TestClass {
 	public static void main(String args[]) {
 		System.out.println("Fun with Git"); //hi hello testing
+		System.out.println("Test Pushing."); //test pushing
 	}
 }
