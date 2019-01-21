e1c004a264dc65f4f9a5d93bdafe6854ef3832a5
AnasIshtiaque <anas.ishtiaque@outlook.com>
2019-01-21 13:17:19 -0500

    branch test

--- src/TalkBoxSimulator.java
+++ src/TalkBoxSimulator.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class TalkBoxSimulator extends JFrame {
 
     public static int i;
-    int ab = 0;
+    int a = 0;
 
     ArrayList<JButton> buttons = new ArrayList<JButton>();
     ArrayList<PlayListener> listeners = new ArrayList<PlayListener>();
