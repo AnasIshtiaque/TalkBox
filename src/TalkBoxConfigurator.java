e8f8041c801ce0b9a9942e571d8e74884e670969
Anas Ishtiaque <anas.ishtiaque@outlook.com>
2019-01-28 12:09:23 -0500

    drag and drop image implementation added

--- src/TalkBoxConfigurator.java
+++ src/TalkBoxConfigurator.java
@@ -1,68 +1,92 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
 import java.awt.dnd.DropTarget;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TalkBoxConfigurator extends JFrame {
 
    ArrayList<JButton> buttons = new ArrayList<JButton>();
    int counter = 0;
-    JLabel iL = new JLabel();
-    JLabel pL = new JLabel();
 
     public TalkBoxConfigurator() {
 
         setVisible(true);
-        setSize(400,200);
+        setSize(300,200);
         setLayout(new GridLayout(2,1,4,4));
 
         JButton button = new JButton("Add");
         add(button);
+
         button.addActionListener(new PlayListener());
-
-/*      add(iL);
-        iL.setSize(100,100);
-        add(pL);
-        pL.setSize(100,100);
-
-        connectToDragDrop();*/
-
-    }
-
-    private void connectToDragDrop(){
-
-        DragListener d = new DragListener(iL, pL);
-
-        new DropTarget(this,d);
 
     }
 
     public class PlayListener implements ActionListener {
         public void actionPerformed(ActionEvent event) {
             buttons.add(new JButton("button" + counter));
+            buttons.get(counter).setTransferHandler(new TalkBoxConfigurator.ImageTransferHandler());
             add(buttons.get(counter));
-            buttons.get(counter).addActionListener(new PlayListener1());
             counter++;
             revalidate();
         }
     }
 
-    public class PlayListener1 implements ActionListener {
+    public static class ImageTransferHandler extends TransferHandler {
 
-        public void actionPerformed(ActionEvent event){
+        public static final DataFlavor[] SUPPORTED_DATA_FLAVORS = new DataFlavor[]{
+                DataFlavor.javaFileListFlavor,
+                DataFlavor.imageFlavor
+        };
 
-            for(int k = 0; k < counter; k++){
-
-                if(event.getSource().equals(buttons.get(k))){
-
-
-
+        @Override
+        public boolean canImport(TransferHandler.TransferSupport support) {
+            boolean canImport = false;
+            for (DataFlavor flavor : SUPPORTED_DATA_FLAVORS) {
+                if (support.isDataFlavorSupported(flavor)) {
+                    canImport = true;
+                    break;
                 }
             }
+            return canImport;
         }
 
+        @Override
+        public boolean importData(TransferHandler.TransferSupport support) {
+            boolean accept = false;
+            if (canImport(support)) {
+                try {
+                    Transferable t = support.getTransferable();
+                    Component component = support.getComponent();
+                    if (component instanceof JButton) {
+                        Image image = null;
+                        if (support.isDataFlavorSupported(DataFlavor.imageFlavor)) {
+                            image = (Image) t.getTransferData(DataFlavor.imageFlavor);
+                        } else if (support.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                            java.util.List files = (List) t.getTransferData(DataFlavor.javaFileListFlavor);
+                            if (files.size() > 0) {
+                                image = ImageIO.read((File) files.get(0));
+                            }
+                        }
+                        ImageIcon icon = null;
+                        if (image != null) {
+                            icon = new ImageIcon(image);
+                        }
+                        ((JButton) component).setIcon(icon);
+                        accept = true;
+                    }
+                } catch (Exception exp) {
+                    exp.printStackTrace();
+                }
+            }
+            return accept;
+        }
     }
 
 }
