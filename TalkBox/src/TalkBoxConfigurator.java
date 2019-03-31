b8d07f6a6ad8c74e94856daadc3c9c30692bf044
Anas Ishtiaque <anas.ishtiaque@outlook.com>
2019-03-30 23:11:38 -0400

    implemented MVC model to configurator

--- TalkBox/src/TalkBoxConfigurator.java
+++ TalkBox/src/TalkBoxConfigurator.java
@@ -4,59 +4,40 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.security.CodeSource;
 import java.util.ArrayList;
-import java.util.List;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-
-
 public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration {
 	
-	
-	//
 	private JButton buttonRecord = new JButton("Record");
-	private SoundRecordingUtil recorder = new SoundRecordingUtil();
-	private Thread playbackThread;
-
+	public static SoundRecordingUtil recorder = new SoundRecordingUtil();
 	private boolean isRecording = false;
 
 	// Icons used for buttons
 	private ImageIcon iconRecord = new ImageIcon(getClass().getResource("Record.gif"));
 	private ImageIcon iconStop = new ImageIcon(getClass().getResource("Stop.gif"));
-//
 	static ArrayList<JButton> sound_buttons = new ArrayList<JButton>();
 	static ArrayList<JButton> img_buttons = new ArrayList<JButton>();
-	List<String> filenames = new ArrayList<String>();
-	List<String> images = new ArrayList<String>();
-	Path RRfilenames;
-	StringBuilder builder = new StringBuilder();
-	int counter = 0;
-	int RecordCounter = 0;
-	String filename = "";
-	ArrayList<StringBuilder> builders = new ArrayList<StringBuilder>();
+	ConfiguratorController cc = new ConfiguratorController();
+	static StringBuilder builder = new StringBuilder();
+	
+	public static int counter = 0;
+	static int RecordCounter = 0;
 	int height = 300;
 	int width = 600;
-	CodeSource cs = TalkBoxConfigurator.class.getProtectionDomain().getCodeSource();
-	File jF = new File(cs.getLocation().toURI().getPath());
-	String jDirectory = jF.getParentFile().getPath() + "/config";
-	static ArrayList<String> saveFilePaths = new ArrayList<String>();
-	private String saveFilePath;
 
 	public TalkBoxConfigurator() throws URISyntaxException, IOException {
+
 		super("Configuration Window");
+		cc.createDirectories();
 		JPanel a = new JPanel();
 		a.setLayout(new BoxLayout(a, 1));
 		add(a);
@@ -66,9 +47,6 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 		JButton button = new JButton("Add");
 		a.add(button);
 		button.addActionListener(new PlayListener());
-		Files.createDirectories(Paths.get(jDirectory + "/audio"));
-		Files.createDirectories(Paths.get(jDirectory + "/images"));
-		Files.createDirectories(Paths.get(jDirectory + "/serialize"));
 		JButton reset = new JButton("Reset");
 		reset.addActionListener(new PlayListener4());
 		a.add(buttonRecord);
@@ -78,45 +56,37 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 		// add(buttonRecord);
 		buttonRecord.addActionListener(new PlayListener2());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 	
 	public class PlayListener4 implements ActionListener {
+		
 		public void actionPerformed(ActionEvent event) {
-			try {
-				TalkBoxConfigurator reset = new TalkBoxConfigurator();
-				dispose();
-				File AudioDirectory = new File(jDirectory + "/audio");
-				for(File f: AudioDirectory.listFiles()) 
-					  f.delete();
-				File IconDirectory = new File(jDirectory + "/images");
-				for(File f: IconDirectory.listFiles()) 
-					  f.delete();
-			} catch (URISyntaxException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
+			cc.resetDirectories();
+			dispose();
+
 		}
+		
 	}
 
 	public class PlayListener2 implements ActionListener {
+		
 		public void actionPerformed(ActionEvent event) {
+			
 			JButton button = (JButton) event.getSource();
+			
 			if (button == buttonRecord) {
+				
 				if (!isRecording) {
+					
 					startRecording();
 					setSize(width += 80, height);
-
-					// builder = new StringBuilder();
 					img_buttons.add(new JButton("Drag image file... "));
 					add(img_buttons.get(counter));
-
 					sound_buttons.add(new JButton("Press "));
 					sound_buttons.get(counter).addActionListener(new PlayListener3());
-					getContentPane().add(new javax.swing.JScrollPane(sound_buttons.get(counter)),
-							java.awt.BorderLayout.CENTER);
+					getContentPane().add(new javax.swing.JScrollPane(sound_buttons.get(counter)), java.awt.BorderLayout.CENTER);
 
 					new FileDrop(System.out, img_buttons.get(counter), /* dragBorder, */ new FileDrop.Listener() {
 
@@ -125,13 +95,9 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 							for (int i = 0; i < files.length; i++) {
 
 								try {
-
-									images.add(counter - 1, files[i].getCanonicalPath());
-									File source = new File(files[i].getCanonicalPath());
-									File dest = new File(
-											jDirectory + "/images/img" + counter + "." + getFileExtension(source));
-									Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
-									ImageIcon img = new ImageIcon("" + images.get(counter - 1));
+									
+									cc.imagesFileDrop(files, i);
+									ImageIcon img = new ImageIcon("" + cc.images.get(counter - 1));
 									img_buttons.get(counter - 1).setIcon(img);
 									img_buttons.get(counter - 1).setText(files[i].getName());
 									img_buttons.get(counter - 1).setHorizontalTextPosition(JLabel.CENTER);
@@ -142,25 +108,33 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 								catch (java.io.IOException e) {
 
 								}
+								
 							}
+							
 							// end for: through each dropped file
 						} // end filesDropped
 
 					});
 
 				} else {
+					
 					stopRecording();
 
 				}
+				
 			}
 
 		}
 
 		private void startRecording() {
+			
 			Thread recordThread = new Thread(new Runnable() {
+				
 				@Override
 				public void run() {
+					
 					try {
+						
 						isRecording = true;
 						buttonRecord.setText("Stop");
 						buttonRecord.setIcon(iconStop);
@@ -168,11 +142,16 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 						recorder.start();
 
 					} catch (LineUnavailableException ex) {
+						
 
 						ex.printStackTrace();
+						
 					}
+					
 				}
+				
 			});
+			
 			recordThread.start();
 
 		}
@@ -181,70 +160,65 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 		 * Stop recording and save the sound into a WAV file
 		 */
 		private void stopRecording() {
+			
 			isRecording = false;
+			
 			try {
+				
 				buttonRecord.setText("Record");
 				buttonRecord.setIcon(iconRecord);
-
 				setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-
 				recorder.stop();
-
 				setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-
 				saveFile();
 
 			} catch (IOException ex) {
 
 				ex.printStackTrace();
+				
 			}
+			
 		}
 
 		/**
 		 * Save the recorded sound into a WAV file.
 		 */
 		private void saveFile() {
-			saveFilePath = jDirectory + "/audio/" + RecordCounter + ".wav";
-
-			File wavFile = new File(saveFilePath);
-			saveFilePaths.add(saveFilePath);
-			filenames.add(saveFilePath);
-			// System.out.println(saveFilePaths.get(RecordCounter));
-
-			try {
-				recorder.save(wavFile);
-
-			} catch (IOException ex) {
-
-				ex.printStackTrace();
-			}
+		
+			cc.save();
 			RecordCounter++;
 			counter++;
+			
 		}
+		
 	}
 
 	public class PlayListener3 implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
-			System.out.println(saveFilePaths.get(0));
+			
+			System.out.println(ConfiguratorController.saveFilePaths.get(0));
+			
 			for (int k = 0; k < counter; k++) {
+				
 				for (int j = 0; j < RecordCounter; j++) {
-					if (e.getSource().equals(sound_buttons.get(k)) && filenames.get(k).equals(saveFilePaths.get(j))) {
+					
+					if (e.getSource().equals(sound_buttons.get(k)) && cc.filenames.get(k).equals(ConfiguratorController.saveFilePaths.get(j))) {
 
 						try {
 
-							File soundFile = new File(saveFilePaths.get(j)); // you could also get the sound file with
+							File soundFile = new File(ConfiguratorController.saveFilePaths.get(j)); // you could also get the sound file with
 																				// an URL
 							AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
-							System.out.println(saveFilePaths.get(j));
+							System.out.println(ConfiguratorController.saveFilePaths.get(j));
+							
 							// Get a sound clip resource.
 							Clip clip = AudioSystem.getClip();
-
 							// Open audio clip and load samples from the audio input stream.
 							clip.open(audioIn);
 							clip.start();
-
 							getAudioFileNames();
+							
 							// getRelativePathToAudioFiles();
 
 						} catch (UnsupportedAudioFileException e1) {
@@ -273,9 +247,9 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 	public class PlayListener implements ActionListener {
 
 		public void actionPerformed(ActionEvent event) {
+			
 			// int NewCounter = counter+RecordCounter;
 			setSize(width += 80, height);
-
 			builder = new StringBuilder();
 			img_buttons.add(new JButton("Drag image file... "));
 			add(img_buttons.get(counter));
@@ -290,32 +264,35 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 					for (int i = 0; i < files.length; i++) {
 
 						try {
-							builder.append(files[i].getCanonicalPath() + "\n");
-							filename = "" + files[i].getCanonicalPath();
-							RRfilenames = files[i].toPath().getParent();
-							filenames.add(filename);
-							builders.add(builder);
-							File source = new File(filename);
-							File dest = new File(jDirectory + "/audio/aud" + counter + "." + getFileExtension(source));
-							Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
-
+							
+							cc.audioFileDrop(files, i);
+							File source = new File(files[i].getCanonicalPath());
 							sound_buttons.get(counter - 1).setText(files[i].getName());
 							sound_buttons.get(counter - 1).setIcon(new ImageIcon("icon/sound.png"));
 							sound_buttons.get(counter - 1).setHorizontalTextPosition(JLabel.CENTER);
 							sound_buttons.get(counter - 1).setVerticalTextPosition(JLabel.BOTTOM);
 							sound_buttons.get(counter - 1).addActionListener(new ActionListener() {
+								
 								@Override
 								public void actionPerformed(ActionEvent e) {
+									
 									try {
+										
 										AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(source);
 										Clip clip = AudioSystem.getClip();
 										clip.open(audioInputStream);
 										clip.start();
+										
 									} catch (Exception e1) {
+										
 										e1.printStackTrace();
+										
 									}
+									
 								}
+								
 							});
+							
 						} // end try
 
 						catch (java.io.IOException e) {
@@ -335,12 +312,9 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 					for (int i = 0; i < files.length; i++) {
 
 						try {
-
-							images.add(counter - 1, files[i].getCanonicalPath());
-							File source = new File(files[i].getCanonicalPath());
-							File dest = new File(jDirectory + "/images/img" + counter + "." + getFileExtension(source));
-							Files.copy(source.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
-							ImageIcon img = new ImageIcon("" + images.get(counter - 1));
+							
+							cc.imageFileDrop(files, i);
+							ImageIcon img = new ImageIcon("" + cc.images.get(counter - 1));
 							img_buttons.get(counter - 1).setIcon(img);
 							img_buttons.get(counter - 1).setText(files[i].getName());
 							img_buttons.get(counter - 1).setHorizontalTextPosition(JLabel.CENTER);
@@ -358,10 +332,10 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 			});
 
 			counter++;
-
 			revalidate();
 
 		}
+		
 	}
 
 	public class PlayListener1 implements ActionListener {
@@ -371,11 +345,11 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 			for (int k = 0; k < counter; k++) {
 
 				if (e.getSource().equals(sound_buttons.get(k))
-						&& builders.get(k).toString().equals(sound_buttons.get(k).getText())) {
+						&& cc.builders.get(k).toString().equals(sound_buttons.get(k).getText())) {
 
 					try {
 
-						File soundFile = new File(filenames.get(k)); // you could also get the sound file with an URL
+						File soundFile = new File(cc.filenames.get(k)); // you could also get the sound file with an URL
 						AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
 
 						// Get a sound clip resource.
@@ -413,45 +387,45 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 	@Override
 	public int getNumberOfAudioButtons() {
 
-		// TODO Auto-generated method stub
 		return counter;
+		
 	}
 
 	@Override
 	public int getNumberOfAudioSets() {
 
-		// TODO Auto-generated method stub
 		return counter;
 	}
 
 	@Override
 	public int getTotalNumberOfButtons() {
 
-		// TODO Auto-generated method stub
 		return counter + 1;
 	}
 
 	@Override
 	public Path getRelativePathToAudioFiles() {
 
-		// TODO Auto-generated method stub
-		return RRfilenames;
+		return cc.RRfilenames;
 	}
 
 	public static void main(String args[]) throws IOException, URISyntaxException {
 
 		TalkBoxConfigurator talkBoxConf = new TalkBoxConfigurator();
 
-
         // Add window listener by implementing WindowAdapter class to
         // the frame instance. To handle the close event we just need
         // to implement the windowClosing() method.
 		talkBoxConf.addWindowListener(new WindowAdapter() {
+			
             @Override
             public void windowClosing(WindowEvent e) {
+            	
                 System.out.println("WindowClosingDemo.windowClosing");
                 System.exit(0);
+                
             }
+            
         });
 
         // Show the frame
@@ -459,24 +433,12 @@ public class TalkBoxConfigurator extends JFrame implements TalkBoxConfiguration 
 
 	}
 
-	private static String getFileExtension(File file) {
-
-		String fileName = file.getName();
-
-		if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0) {
-
-			return fileName.substring(fileName.lastIndexOf(".") + 1);
-		}
-
-		else
-			return "";
-
-	}
 
 	@Override
 	public String[][] getAudioFileNames() {
-		// TODO Auto-generated method stub
+		
 		return null;
+		
 	}
 
 }
